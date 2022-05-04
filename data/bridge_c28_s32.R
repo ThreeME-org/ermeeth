070bc874a40e49e76e8b9481177c361e1cc8dcd6
PaulMalliet <paul.malliet@sciencespo.fr>
2022-05-04 16:13:52 +0200

    Create bridge_c28_s32.R
    Add of bridge in the data set (to be tested)

---
+++ data/bridge_c28_s32.R
@@ -0,0 +1,23 @@
+bridge_sectors <-list(
+  "Agriculture" = c("sagr", "sfor"),
+  "Energy intensive industry" = c("sveh", "spap",	"sche", "spla", "smet","sgla"),
+  "Other industry" = c("sfoo", "sigo", "scgo"),
+  "Construction" = c("scon"),
+  "Transport" = c("srai",	"sroa",	"swat",	"sair"),
+  "Services" = c("spri", "spub"),
+  "Fossil based energy" = c("smin", "soil", "sgas", "seoi",	"sega", "seco"),
+  "Other energy" = c("sbfu", "sbga", "senu", "sewi", "seso",	"sehy",	"sech",	"seot")
+)
+
+
+bridge_commodities <- list(
+  "Agriculture" = c("cagr", "cfor"),
+  "Energy intensive industry" = c("cveh", "cpap",	"cche", "cpla", "cmet","cgla"),
+  "Other industry" = c("cfoo", "cigo", "ccgo"),
+  "Construction" = c("ccon"),
+  "Transport" = c("crai",	"croa",	"cwat",	"cair"),
+  "Services" = c("cpri", "cpub"),
+  "Fossil based energy" = c("cmin", "ccoa", "ccoi", "cfut", "cfuh", "cgas"),
+  "Other energy" = c("cele", "chea", "cbio", "cote")
+)
+
