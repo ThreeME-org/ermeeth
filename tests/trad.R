c98302caf2dc3c4270131297a3ca7da5bfd32b0f
Lucia Sequeira <lucia.sequeira@eleves.enpc.fr>
2023-08-24 14:18:52 +0200

    Create trad.R

---
+++ tests/trad.R
@@ -0,0 +1,11 @@
+trad <- function(x,data = read.csv(file.path("src","translation_file.csv"),sep = ";", encoding = "UTF-8"),lang = language){
+  if(exists("wd_racine")==TRUE){data = read.csv(file.path("results","report","markdown","src","translation_file.csv"),sep = ";", encoding = "UTF-8")}
+  names(data)[1] <- "en"
+  i = 1
+  nb = length(x)
+  for(i in 1:nb){
+    x[i] <- data %>% filter(en == x[i]) %>% select(all_of(lang))
+  }
+  x <- unlist(x)
+  x
+}
