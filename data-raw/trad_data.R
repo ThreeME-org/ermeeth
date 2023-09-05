0e12a93f38408889e1cb9efb6ebc7e599cf72b2d
anissaOFCE <anissa.saumtally@sciencespo.fr>
2023-09-05 11:57:30 +0200

    new version generating trad data base

--- data-raw/trad_data.R
+++ data-raw/trad_data.R
@@ -1,3 +1,9 @@
 trad_language_base  <- read.csv("tests/translation_file.csv", sep = ";")
+Encoding(trad_language_base$fr) <- "latin1"
+trad_language_base$fr <- iconv(
+trad_language_base$fr,
+  "latin1",
+  "UTF-8"
+)
 
 usethis::use_data(trad_language_base, overwrite = TRUE)
