ae05501a1913728ce97461a51cacdcbd03c1a137
Lucia Sequeira <lucia.sequeira@eleves.enpc.fr>
2023-09-05 12:31:59 +0200

    Update trad_data.R

--- data-raw/trad_data.R
+++ data-raw/trad_data.R
@@ -5,5 +5,11 @@ trad_language_base$fr,
   "latin1",
   "UTF-8"
 )
+Encoding(trad_language_base$es) <- "latin1"
+trad_language_base$es <- iconv(
+  trad_language_base$es,
+  "latin1",
+  "UTF-8"
+)
 
 usethis::use_data(trad_language_base, overwrite = TRUE)
